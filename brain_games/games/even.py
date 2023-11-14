49e13a5b01ce680d81ffca65ae9f9697304c45ad
Aleksei <jiunov@gmail.com>
2023-11-14 21:27:59 +0600

    4

---
+++ brain_games/games/even.py
@@ -0,0 +1,5 @@
+# from random import randint
+
+
+def even_():
+    print('working')
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
+from brain_games.games.even import even_
+
+
+def main():
+    welcome_user()
+    even_()
+
+
+if __name__ == '__main__':
+    main()
